2778f26f72629d24e094afba3604efdcdf5d3c6e
Daniil <ryazapovdanii@gmail.com>
2023-04-26 16:50:37 +0300

    подолжение прошлого коммита

--- Toy.java
+++ Toy.java
@@ -7,4 +7,37 @@ public class Toy {
     private String id;
     private String name;
     private double frequency;
+
+
+     public Toy(String id, String name, double frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+
 }
