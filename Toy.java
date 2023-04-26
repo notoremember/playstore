fbb17ac5e07d7ce81984c3cd6c0a44645808808c
Daniil <ryazapovdanii@gmail.com>
2023-04-26 16:46:53 +0300

    импорт всех api и создание основного класса Toy

---
+++ Toy.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Toy {
+    private String id;
+    private String name;
+    private double frequency;
+}
